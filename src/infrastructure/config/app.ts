b9e919e1a02462c9d068c26a3fc4dce534465907
Sayand2002 <sayandak2002@gmail.com>
2024-08-14 19:46:35 +0530

    initial commit

---
+++ src/infrastructure/config/app.ts
@@ -0,0 +1,23 @@
+import 'dotenv/config'
+
+import express, { NextFunction, Request, Response } from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import morgan from 'morgan';
+import errorHandler from '../../usecase/handler/errorHandler';
+
+export const app = express();
+
+app.use(cookieParser());
+
+app.use(express.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }))
+
+app.use(cors({credentials: true}))
+
+app.use(morgan('combined'));
+
+
+
+
+// Use error handler
+app.use(errorHandler);
